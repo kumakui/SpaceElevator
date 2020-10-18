9a1bd1d0990b7bbd6d5ef6d9095219fd898f9d8e
Ryosuke Tanaka <kumakui@outlook.com>
2020-10-19 03:33:36 +0900

    0.0.24
    燃料システムを追加
    ボス戦再開時にボスのhpバーが全快しない不具合修正

---
+++ Assets/Scripts/View/UI/FuelBarUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelBarUI : MonoBehaviour
+{
+    public ElevatorController elevatorController;
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        elevatorController.elevatorFuelChangeAction += OnFuelChange;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnFuelChange(float fuel)
+    {
+        slider.value = fuel / 100;
+    }
+}
