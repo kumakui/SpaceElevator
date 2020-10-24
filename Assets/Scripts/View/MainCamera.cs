4a6ab51884b566fbdacc1d6fc1994ef1692b4aa4
Ryosuke Tanaka <kumakui@outlook.com>
2020-10-24 17:13:22 +0900

    0.0.29
    missingになっていたtreeを削除

--- Assets/Scripts/View/MainCamera.cs
+++ Assets/Scripts/View/MainCamera.cs
@@ -16,9 +16,13 @@ public class MainCamera : MonoBehaviour
 
     private void Awake()
     {
-        var scaleWidth = (Screen.height / this.baseHeight) * (this.baseWidth / Screen.width);
-        var scaleRatio = Mathf.Max(scaleWidth, 1.0f);
-        camera.fieldOfView = Mathf.Atan(Mathf.Tan(this.camera.fieldOfView * 0.5f * Mathf.Deg2Rad) * scaleRatio) * 2.0f * Mathf.Rad2Deg;
+        //アスペクト比固定
+        var scale = Mathf.Min(Screen.height / this.baseHeight, Screen.width / this.baseWidth);
+        var width = (this.baseWidth * scale) / Screen.width;
+        var height = (this.baseHeight * scale) / Screen.height;
+        this.camera.rect = new Rect((1.0f - width) * 0.5f, (1.0f - height) * 0.5f, width, height);
+
+
     }
 
     // Start is called before the first frame update
